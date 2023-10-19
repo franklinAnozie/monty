c8979ab103867afae0895b37c701db90eebc4f35
franklinAnozie <cfanozie@gmail.com>
2023-10-19 04:14:38 +0100

    fixed rotl

--- extra_opcodes.c
+++ extra_opcodes.c
@@ -127,12 +127,13 @@ void rotl(stack_t **stack, unsigned int line_number)
 	(void)line_number;
 	if (*stack == NULL || (*stack)->next == NULL)
 		return;
-	temp = *stack;
-	next = temp->next;
+	temp = next = *stack;
 	while (temp->next != NULL)
 		temp = temp->next;
+	next = next->next;
+	next->prev = NULL;
 	temp->next = *stack;
 	(*stack)->prev = temp;
+	(*stack)->next = NULL;
 	*stack = next;
-	(*stack)->prev = NULL;
 }
